389239916e502b406cba0a0dcd6e5988b971f7ec
Chris Joakim <cjoakim@bellsouth.net>
2013-08-25 07:53:20 -0400

    Created and using the private data directory now.

--- lib/tasks/qiflib.rake
+++ lib/tasks/qiflib.rake
@@ -1,23 +1,23 @@
-=begin 
+=begin
 
 Copyright (C) 2013 Chris Joakim.
 
 =end
 
 desc 'Build the gem, then unpack and validate it'
-task :build do 
+task :build do
   filename = "qiflib-#{Qiflib::VERSION}.gem"
   dirname  = "qiflib-#{Qiflib::VERSION}"
-  
+
   puts "Removing #{filename} ..."
   `rm  build/#{filename}`
-  
+
   puts "Building #{filename} ..."
-  `gem build qiflib.gemspec` 
-  
+  `gem build qiflib.gemspec`
+
   puts "Copying gem to build dir ..."
-  `cp  #{filename} build/` 
-  
+  `cp  #{filename} build/`
+
   puts "Moving the unpack directory #{dirname} ..."
   `rm -rf build/#{dirname}`
 
@@ -26,14 +26,14 @@ task :build do
 end
 
 desc 'Push the gem to RubyGems.org'
-task :push do 
+task :push do
   filename = "qiflib-#{Qiflib::VERSION}.gem"
   `gem push #{filename}`
 end
 
 desc 'Convert qif file(s) to csv format.'
 task :qif2csv do
-  f = command_line_arg('f', 'data/ibank_20120328.qif')
+  f = command_line_arg('f', 'data/private/ibank_20120328.qif')
   csv_lines = Qiflib::Util.qiftocsv([f])
   puts "csv_lines: #{csv_lines.size}"
   csv_lines.each { | csv | puts csv }
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -11,22 +11,50 @@ require File.dirname(__FILE__) + '/../lib/qiflib.rb'
 require 'spec_helper'
 
 describe "Qiflib::Util" do
-  
+
+  def test_qif_filename
+    'data/private/ibank_20120328.qif'
+  end
+
+  def test_transactions_csv_filename
+    'data/private/qiflib_transactions.csv'
+  end
+
+  def test_transactions_txt_filename
+    'data/private/qiflib_transactions.txt'
+  end
+
+  def test_categories_csv_filename
+    'data/private/qiflib_categories.csv'
+  end
+
+  def test_categories_txt_filename
+    'data/private/qiflib_categories.txt'
+  end
+
+  def test_ddl_filename
+    'data/qiflib.ddl'
+  end
+
+  def test_sh_filename
+    'data/qiflib.sh'
+  end
+
   it "should implement method 'transactions_to_csv', for ibank files" do
-    csv_filename = 'data/qiflib_transactions.csv' 
+    csv_filename = test_transactions_csv_filename
     expected_line_count = 9945
     expected_field_count = 28
     input_list = []
-    input_list << { 
+    input_list << {
       :owner    => 'Chris',
-      :filename => 'data/ibank_20120328.qif',
+      :filename => test_qif_filename,
       :source   => Qiflib::SOURCE_IBANK }
     csv_lines = Qiflib::Util.transactions_to_csv(input_list)
     write_lines(csv_filename, csv_lines)
     csv_lines.size.should == expected_line_count
     csv_in = IO.readlines(csv_filename)
     csv_in.size.should == expected_line_count
-    
+
     header_array = CSV.parse(csv_in[0])[0]
     first_tran_array = CSV.parse(csv_in[1])[0]
     last_tran_array  = CSV.parse(csv_in[-1])[0]
@@ -62,7 +90,7 @@ describe "Qiflib::Util" do
     first_tran_array[25].should == ''
     first_tran_array[26].should == ''
     first_tran_array[27].should == 'x'
-           
+
     last_tran_array.size.should == expected_field_count
     last_tran_array[0].should == '9944'
     last_tran_array[1].should == 'chris'
@@ -92,23 +120,23 @@ describe "Qiflib::Util" do
     last_tran_array[25].should == ''
     last_tran_array[26].should == ''
     last_tran_array[27].should == 'x'
-  end 
-  
+  end
+
   it "should implement method 'transactions_to_delim', for ibank files" do
-    delim_filename = 'data/qiflib_transactions.txt' 
+    delim_filename = test_transactions_txt_filename
     expected_line_count = 9944
     expected_field_count = 28
     input_list = []
-    input_list << { 
+    input_list << {
       :owner    => 'Chris',
-      :filename => 'data/ibank_20120328.qif',
+      :filename => test_qif_filename,
       :source   => Qiflib::SOURCE_IBANK }
     delim_lines = Qiflib::Util.transactions_to_delim(input_list)
     write_lines(delim_filename, delim_lines)
     delim_lines.size.should == expected_line_count
     delim_in = IO.readlines(delim_filename)
     delim_in.size.should == expected_line_count
-    
+
     first_tran_array = delim_in[0].strip.split('^')
     last_tran_array  = delim_in[-1].strip.split('^')
 
@@ -141,7 +169,7 @@ describe "Qiflib::Util" do
     first_tran_array[25].should == ''
     first_tran_array[26].should == ''
     first_tran_array[27].should == 'x'
-           
+
     last_tran_array.size.should == expected_field_count
     last_tran_array[0].should == '9944'
     last_tran_array[1].should == 'chris'
@@ -171,16 +199,16 @@ describe "Qiflib::Util" do
     last_tran_array[25].should == ''
     last_tran_array[26].should == ''
     last_tran_array[27].should == 'x'
-  end 
+  end
 
   it "should implement method 'catetory_names_to_csv', for ibank files" do
-    csv_filename = 'data/qiflib_categories.csv'
-    csv_lines = Qiflib::Util.catetory_names_to_csv(['data/ibank_20120328.qif'])
+    csv_filename = test_categories_csv_filename
+    csv_lines = Qiflib::Util.catetory_names_to_csv([test_qif_filename])
     write_lines(csv_filename, csv_lines)
     csv_lines.size.should == 88
     csv_in = IO.readlines(csv_filename)
     csv_in.size.should == 88
-    
+
     header_array    = CSV.parse(csv_in[0])[0]
     first_cat_array = CSV.parse(csv_in[1])[0]
     last_cat_array  = CSV.parse(csv_in[-1])[0]
@@ -189,57 +217,56 @@ describe "Qiflib::Util" do
 
     first_cat_array.size.should == 2
     first_cat_array[0].should == '1'
-    first_cat_array[1].should == '001 salary' 
+    first_cat_array[1].should == '001 salary'
 
     last_cat_array.size.should == 2
     last_cat_array[0].should == '87'
     last_cat_array[1].should == '_qk bal to stmt'
   end
-  
+
   it "should implement method 'catetory_names_to_delim', for ibank files" do
-    delim_filename = 'data/qiflib_categories.txt'
-    delim_lines = Qiflib::Util.catetory_names_to_delim(['data/ibank_20120328.qif'])
+    delim_filename = test_categories_csv_filename
+    delim_lines = Qiflib::Util.catetory_names_to_delim([test_qif_filename])
     write_lines(delim_filename, delim_lines)
     delim_lines.size.should == 87
     delim_in = IO.readlines(delim_filename)
     delim_in.size.should == 87
-    
+
     first_cat_array = delim_in[0].strip.split('^')
     last_cat_array  = delim_in[-1].strip.split('^')
 
     first_cat_array.size.should == 2
     first_cat_array[0].should == '1'
-    first_cat_array[1].should == '001 salary' 
+    first_cat_array[1].should == '001 salary'
 
     last_cat_array.size.should == 2
     last_cat_array[0].should == '87'
-    last_cat_array[1].should == '_qk bal to stmt' 
+    last_cat_array[1].should == '_qk bal to stmt'
   end
-  
+
   it "should implement method 'generate_sqlite_ddl'" do
-    ddl_filename = 'data/qiflib.ddl'
+    ddl_filename = test_ddl_filename
     ddl_lines = Qiflib::Util.generate_sqlite_ddl
     write_lines(ddl_filename, ddl_lines, true)
     ddl_lines.size.should == 45
     ddl_in = IO.readlines(ddl_filename)
     ddl_in.size.should == 45
-    
+
     ddl_in[1].strip.should == 'drop table if exists transactions;'
     ddl_in[2].strip.should == 'drop table if exists categories;'
-    ddl_in[4].strip.should == 'create table transactions('  
+    ddl_in[4].strip.should == 'create table transactions('
   end
-  
+
   it "should implement method 'generate_sqlite_load_script'" do
-    sh_filename = 'data/qiflib.sh'
+    sh_filename = test_sh_filename
     sh_lines = Qiflib::Util.generate_sqlite_load_script
     write_lines(sh_filename, sh_lines, true)
     sh_lines.size.should == 4
     sh_in = IO.readlines(sh_filename)
     sh_in.size.should == 4
-    
+
     sh_in[0].strip.should == '#!/bin/bash'
     sh_in[2].strip.should == 'sqlite3 qiflib.db < qiflib.ddl'
-  end 
+  end
 
 end
-   
